4a30fd58806c61e9877173e71757a6dabf5319be
Semsei <guillaume@36pixels.fr>
2023-05-16 11:41:43 +0200

    dev - get categories movies and fullfuill modale

--- scripts/classes/request.js
+++ scripts/classes/request.js
@@ -1,33 +1,21 @@
-export async function api_request(atts) {
-  // const result = {
-  //   "id": 9,
-  //   "url": "http://localhost:8000/api/v1/titles/9",
-  //   "imdb_url": "https://www.imdb.com/title/tt0000009/",
-  //   "title": "Miss Jerry",
-  //   "year": 1894,
-  //   "imdb_score": "6.0",
-  //   "votes": 154,
-  //   "image_url": "https://m.media-amazon.com/images/M/MV5BNjA5Y2ZhYzctNDc1Yy00OGViLWI3NGUtOTYwZmE3NDFiYmIxXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_UY268_CR72,0,182,268_AL_.jpg",
-  //   "directors": [
-  //       "Alexander Black"
-  //   ],
-  //   "actors": [
-  //       "Blanche Bayliss",
-  //       "Chauncey Depew",
-  //       "William Courtenay"
-  //   ],
-  //   "writers": [
-  //       "Alexander Black"
-  //   ],
-  //   "genres": [
-  //       "Romance"
-  //   ]
-  // };
+export async function api_request(type, category='no ctegory') {
 
-  let url = 'http://localhost:8000/api/v1/titles/?page_size=1';
+  let url = 'http://localhost:8000/api/v1/';
 
-  switch (atts[0]) {
+  switch (type) {
     case 'highlight':
+      url += 'titles/'
+      url += '?page_size=1';
+      url += '&sort_by=-imdb_score';
+      break;
+
+    case 'category':
+      url += 'titles/'
+      url += '?page_size=7';
+      url += '&genre_contains=';
+      url += category;
+      url += '&sort_by=-imdb_score';
+      break;
 
     default:
       url = 'http://localhost:8000/api/v1/titles/?page_size=1';
--- scripts/main.js
+++ scripts/main.js
@@ -2,22 +2,69 @@ import {Movie} from './classes/movie.js'
 import { api_request } from './classes/request.js';
 
 // Tester le retour de la fonction request
-const atts = [];
-const movies_infos = await api_request(atts);
-const movie_infos = movies_infos[0];
+// let type = '';
+// const atts = [];
+// const movies_infos = await api_request(atts);
+// const movie_infos = movies_infos[0];
 
-// Insérer une image pour le film en highlight
+// Récupérer les informations sur le film en highlight
+let type = 'highlight'
+const request = await api_request(type);
+const highlight_infos = request[0];
+
+// Remplir le html
 const highlight_movie = document.querySelector('.moviesBlock__highlight');
-const movie_img = movie_infos.image_url;
-highlight_movie.style.backgroundImage = `url('${movie_img}')`;
+highlight_movie.style.backgroundImage = `url('${highlight_infos.image_url}')`;
 const highlight_title_tag = document.querySelector('.movieTitle');
-highlight_title_tag.innerHTML = movie_infos.title;
-const modale = document.querySelector('.modale');
+highlight_title_tag.innerHTML = highlight_infos.title;
 
-// Renseigner les catégories souhaitées
+// Préparer le clic hors modale pour fermeture modale
+const out_of_modale = document.querySelector('.outOfModale');
+
+// Créer la fenêtre modale
+const modale = document.querySelector('.modale');
+// remplir la fenetre modale
+function fullfill_modale(movie_infos) {
+    const datas = [
+        ['title', movie_infos.title ? movie_infos.title : 'NC'],
+        ['categories', movie_infos.genres ? movie_infos.genres : 'NC'],
+        ['date', movie_infos.date_published ? movie_infos.date_published : 'NC'],
+        ['rating', movie_infos.votes ? movie_infos.votes : 'NC'],
+        ['score', movie_infos.imdb_score ? movie_infos.imdb_score : 'NC'],
+        ['directors', movie_infos.directors ? movie_infos.directors : 'NC'],
+        ['casting', movie_infos.actors ? movie_infos.actors : 'NC'],
+        ['duration', movie_infos.duration ? movie_infos.duration : 'NC'],
+        ['countries', movie_infos.countries ? movie_infos.countries : 'NC'],
+        ['boxOffice', movie_infos.worldwide_gross_income ? movie_infos.worldwide_gross_income : 'NC'],
+        ['synopsis', movie_infos.description ? movie_infos.description : 'NC']
+    ];
+
+    const picture = modale.querySelector('.modale__content__picture');
+    picture.style.backgroundImage = `url(${movie_infos.image_url})`;
+
+
+    for (let i=0; i<datas.length; i++) {
+        const element = modale.querySelector(`.${datas[i][0]}`);
+        element.innerHTML = datas[i][1];
+    }
+
+    // Activer le bouton close
+    const close = modale.querySelector('.modale__close');
+    close.addEventListener("click", function() {
+        out_of_modale.style.left = '-100%';
+        modale.style.left = '-80%';
+    });
+
+    out_of_modale.addEventListener("click", function() {
+        out_of_modale.style.left = '-100%';
+        modale.style.left = '-80%';
+    });
+}
+
+// Renseigner les catégories souhaitées, accompagnée de leur traduction
 const categories =[
-    'thrillers',
-    'comedies'
+    ['Drama', 'Drames'],
+    ['Comedy','Comedies']
 ]
 
 // Créer categories blocks
@@ -25,14 +72,18 @@ const categories_container = document.querySelector('.moviesBlock__categories');
 
 // créer les blocks de categories
 for (let j=0; j<categories.length; j++) {
+    // Récupérer la liste des films et leurs informations
+    type = 'category';
+    const category_infos = await api_request(type, categories[j][0]);
+
     const category_container = document.createElement('div');
     category_container.classList.add('moviesBlock__categories__category', `${categories[j]}`);
     categories_container.appendChild(category_container);
 
-    // Titres les categories
+    // Titrer les categories
     const title = document.createElement('h2');
     title.classList.add('moviesBlock__categories__category__title');
-    title.innerHTML = categories[j];
+    title.innerHTML = categories[j][1];
     category_container.appendChild(title);
 
     // créer le carousel
@@ -40,34 +91,40 @@ for (let j=0; j<categories.length; j++) {
     carousel_container.classList.add('carouselContainer');
     category_container.appendChild(carousel_container);
 
-    const carousel_content = document.createElement('div');
-    carousel_content.classList.add('carouselContent')
-    carousel_container.appendChild(carousel_content);
+    if (category_infos.length == 0) {
+        carousel_container.innerHTML = 'Aucun film  ne correspond à cette catégorie.';
+    } else {
+        const carousel_content = document.createElement('div');
+        carousel_content.classList.add('carouselContent')
+        carousel_container.appendChild(carousel_content);
 
-    // Ajouter les films dans le carousel
-    for (let k=1; k<7; k++) {
-        // Créer le conteneur
-        const movie_container = document.createElement('div');
-        movie_container.classList.add('movie', `movie_${k-1}`);
+        // Ajouter les films dans le carousel
+        for (let k=0; k<6; k++) {
+            // Créer le conteneur
+            const movie_container = document.createElement('div');
+            movie_container.classList.add('movie', `movie_${k+1}`);
 
-        // Intégrer l'image du film
-        const thumbnail = document.createElement('div');
-        thumbnail.classList.add('thumbnail');
-        movie_container.appendChild(thumbnail);
+            // Intégrer l'image du film
+            const thumbnail = document.createElement('div');
+            thumbnail.classList.add('thumbnail');
+            movie_container.appendChild(thumbnail);
 
-        // Intégrer le titre
-        const title = document.createElement('h3');
-        title.innerHTML = movie_infos.title;
-        movie_container.appendChild(title);
+            // Intégrer le titre
+            const title = document.createElement('h3');
+            title.innerHTML = category_infos[k].title;
+            movie_container.appendChild(title);
 
-        // Intégrer le conteneur dans le carousel
-        carousel_content.appendChild(movie_container);
-        thumbnail.style.backgroundImage = `url('${movie_infos.image_url}')`;
+            // Intégrer le conteneur dans le carousel
+            carousel_content.appendChild(movie_container);
+            thumbnail.style.backgroundImage = `url('${category_infos[k].image_url}')`;
 
-        // Activer la modale au clic sur un film
-        movie_container.addEventListener("click", function() {
-            modale.style.left = '0';
-        });
+            // Activer la modale au clic sur un film
+            movie_container.addEventListener("click", function() {
+                fullfill_modale(category_infos[k]);
+                out_of_modale.style.left = '0';
+                modale.style.left = '0';
+            });
+        }
     }
 
     // créer un repére de position
@@ -102,37 +159,3 @@ for (let j=0; j<categories.length; j++) {
         }
       });
 }
-
-// remplir la fenetre modale
-function fullfill_modale() {
-    const datas = [
-        ['title', movie_infos.title ? movie_infos.title : 'NC'],
-        ['categories', movie_infos.genres ? movie_infos.genres : 'NC'],
-        ['date', movie_infos.date_published ? movie_infos.date_published : 'NC'],
-        ['rating', movie_infos.votes ? movie_infos.votes : 'NC'],
-        ['score', movie_infos.imdb_score ? movie_infos.imdb_score : 'NC'],
-        ['directors', movie_infos.directors ? movie_infos.directors : 'NC'],
-        ['casting', movie_infos.actors ? movie_infos.actors : 'NC'],
-        ['duration', movie_infos.duration ? movie_infos.duration : 'NC'],
-        ['countries', movie_infos.countries ? movie_infos.countries : 'NC'],
-        ['boxOffice', movie_infos.worldwide_gross_income ? movie_infos.worldwide_gross_income : 'NC'],
-        ['synopsis', movie_infos.description ? movie_infos.description : 'NC']
-    ];
-
-    const picture = modale.querySelector('.modale__content__picture');
-    picture.style.backgroundImage = `url(${movie_infos.image_url})`;
-
-
-    for (let i=0; i<datas.length; i++) {
-        const element = modale.querySelector(`.${datas[i][0]}`);
-        element.innerHTML = datas[i][1];
-    }
-
-    // Activer le bouton close
-    const close = modale.querySelector('.modale__close');
-    close.addEventListener("click", function() {
-        modale.style.left = '-80%';
-    });
-}
-
-fullfill_modale();
