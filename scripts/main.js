36772f1fccdaea5dc037a43f0884ecbf671d8d6d
Semsei <guillaume@36pixels.fr>
2023-05-22 15:43:10 +0200

    bugfix - chrome mask image

--- scripts/main.js
+++ scripts/main.js
@@ -137,13 +137,15 @@ for (let j=0; j<categories.length; j++) {
     // Créer les fleches
     const left_arrow = document.createElement('div');
     left_arrow.classList.add('arrow');
-    left_arrow.style.maskImage = 'url(./src/arrow.svg)';
+    left_arrow.style = '-webkit-mask-image: url("./src/arrow.svg");'
+    // left_arrow.style.maskImage = "url(./src/arrow.svg)";
     category_container.appendChild(left_arrow);
     left_arrow.parentNode.insertBefore(left_arrow, carousel_container);
 
     const right_arrow = document.createElement('div');
     right_arrow.classList.add('arrow');
-    right_arrow.style.maskImage = 'url(./src/arrow.svg)';
+    right_arrow.style = '-webkit-mask-image: url("./src/arrow.svg");'
+    // right_arrow.style.maskImage = "url(./src/arrow.svg)";
     right_arrow.style.transform = "scaleX(-1)";
     category_container.appendChild(right_arrow);
 
